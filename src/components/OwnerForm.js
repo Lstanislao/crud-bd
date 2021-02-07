6e12eb09b510ebeb2ef0e7cdb8e9c23a711b854e
Lstanislao <luis.stanislao@correo.unimet.edu.ve>
2021-02-07 17:02:46 -0400

    ahi va

--- src/components/OwnerForm.js
+++ src/components/OwnerForm.js
@@ -1,6 +1,19 @@
 import { Form, Button, Container  } from 'react-bootstrap';
+import { useForm } from '../hooks/useForm';
 
 export const OwnerForm = () => {
+  
+  
+    const [ formValues , handleInputChange, reset] = useForm({
+    name: '',
+    lastName:'',
+    CI:'',
+    cellphone: '',
+    email: '',
+  });
+  
+    const { name, lastName, CI, cellphone,  email }= formValues;
+
   return (
     
     <div>
@@ -10,27 +23,27 @@ export const OwnerForm = () => {
 
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="" />
+            <Form.Control name="name" value={ name } type="text" placeholder="" />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="" />
+            <Form.Control name="lastName" value={ lastName } type="text" placeholder="" />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>CI</Form.Label>
-            <Form.Control type="text"  />
+            <Form.Control name="CI" value={ CI } type="text"  />
           </Form.Group>
           
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Telefono</Form.Label>
-            <Form.Control type="text" placeholder="" />
+            <Form.Control  name="cellphone" value={ cellphone } type="text" placeholder="" />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Correo</Form.Label>
-            <Form.Control type="email" placeholder="" />
+            <Form.Control name="email" value={ email } type="email" placeholder="" />
           </Form.Group>
 
 
